fc9524c1866d6d3718a1b113b5437ed1a4efbfad
baxtiyorovanozima02 <baxtiyorovanozima02>
2026-05-26 09:18:09 +0100

    feat: Connect tests urls to core

--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.jwt')),
     path('api/v1/users/', include('apps.users.urls')),
+    path('api/v1/tests/', include('apps.tests.urls')),
 ]
