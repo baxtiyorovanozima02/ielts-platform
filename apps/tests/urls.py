1308a614dac538ca4b14983ed75cf548d8dc273d
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-05-26 10:51:02 +0100

    feat: Add writing evaluation URL

--- apps/tests/urls.py
+++ apps/tests/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import SectionListView, TestListView, TestDetailView, QuestionListView
+from .views import (
+    SectionListView,
+    TestListView,
+    TestDetailView,
+    QuestionListView,
+    WritingEvaluationView
+)
 
 urlpatterns = [
     path('sections/', SectionListView.as_view(), name='section-list'),
     path('tests/', TestListView.as_view(), name='test-list'),
     path('tests/<int:pk>/', TestDetailView.as_view(), name='test-detail'),
     path('tests/<int:test_id>/questions/', QuestionListView.as_view(), name='question-list'),
+    path('tests/<int:test_id>/evaluate/', WritingEvaluationView.as_view(), name='writing-evaluate'),
 ]
