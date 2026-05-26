f574ae9bdabe797c231b52898a66dc678307359b
baxtiyorovanozima02 <baxtiyorovanozima02>
2026-05-26 09:13:39 +0100

    feat: Add Section, Test, Question views

--- apps/tests/views.py
+++ apps/tests/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Section, Test, Question
+from .serializers import SectionSerializer, TestSerializer, QuestionSerializer
 
-# Create your views here.
+class SectionListView(generics.ListAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class TestListView(generics.ListAPIView):
+    serializer_class = TestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        section = self.request.query_params.get('section')
+        if section:
+            return Test.objects.filter(section__name=section, is_active=True)
+        return Test.objects.filter(is_active=True)
+
+class TestDetailView(generics.RetrieveAPIView):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class QuestionListView(generics.ListAPIView):
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        test_id = self.kwargs.get('test_id')
+        return Question.objects.filter(test_id=test_id)
