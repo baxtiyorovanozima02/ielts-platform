ec4e83becd997c089edbe6e89049aed96fbe7af6
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-05-27 14:14:37 +0100

    feat: add writing and speaking result list URLs

--- apps/tests/urls.py
+++ apps/tests/urls.py
@@ -5,7 +5,9 @@ from .views import (
     TestDetailView,
     QuestionListView,
     WritingEvaluationView,
-    SpeakingEvaluationView
+    WritingResultListView,
+    SpeakingEvaluationView,
+    SpeakingResultListView,
 )
 
 urlpatterns = [
@@ -14,5 +16,7 @@ urlpatterns = [
     path('tests/<int:pk>/', TestDetailView.as_view(), name='test-detail'),
     path('tests/<int:test_id>/questions/', QuestionListView.as_view(), name='question-list'),
     path('tests/<int:test_id>/evaluate/', WritingEvaluationView.as_view(), name='writing-evaluate'),
+    path('results/writing/', WritingResultListView.as_view(), name='writing-results'),
     path('tests/<int:test_id>/speaking/', SpeakingEvaluationView.as_view(), name='speaking-evaluate'),
+    path('results/speaking/', SpeakingResultListView.as_view(), name='speaking-results'),
 ]
