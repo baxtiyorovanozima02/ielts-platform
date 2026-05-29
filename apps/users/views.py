34e30aebdf278c882b7df6e811d3b087fa8e78ef
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-05-29 10:22:07 +0100

    feat: add Swagger description to MeView

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,10 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(operation_summary="Mening profilim")
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
