04199a01f352acdded8588c452bc455a7bc48949
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-05-30 14:00:49 +0100

    feat: configure production settings

--- core/settings.py
+++ core/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@27!+a$=b8mr+!u)b8el_j_ss1gfma!y_58(v-7o^@*)*28h8&'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 
 # Application definition
@@ -190,3 +189,6 @@ INSTALLED_APPS += ['django_celery_results']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
