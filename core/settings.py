4ab6dee5acff2e8bcc8a47f34860832bbb61dd20
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-06-26 10:37:43 +0100

    fix: CSRF trusted origins qoshildi

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     'CSRF_TRUSTED_ORIGINS',
-    'https://*.railway.app,https://*.vercel.app'
+    'https://*.railway.app,https://*.vercel.app,https://api.ieltsera.uz,https://ieltsera.uz'
 ).split(',')
 
 
