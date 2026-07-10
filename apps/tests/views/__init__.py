1375d0b20a3f20dcabe5132e6668919686f42428
baxtiyorovanozima02 <baxtiyorovanozima02@gmail.com>
2026-07-10 15:45:00 +0100

    fix: SpeakingSessionAvatarTokenView ni views __init__ dan eksport qilish

--- apps/tests/views/__init__.py
+++ apps/tests/views/__init__.py
@@ -1,6 +1,16 @@
 from .section import SectionListView, TestListView, TestDetailView, QuestionListView
 from .writing import WritingEvaluationView, WritingResultListView
-from .speaking import SpeakingEvaluationView, SpeakingResultListView
+from .speaking import (
+    SpeakingEvaluationView,
+    SpeakingResultListView,
+    ExaminerVoiceListView,
+    SpeakingSessionStartView,
+    SpeakingSessionAvatarTokenView,
+    SpeakingSessionCurrentQuestionView,
+    SpeakingSessionAnswerView,
+    SpeakingSessionAbandonView,
+    SpeakingSessionResultView,
+)
 from .progress import UserProgressView, DailyPlanView, AIGeneratePlanView
 from .statistics import BandScoreHistoryView, OverallProgressView, WeakAreasView
 from .ai_tutor import AIChatView
